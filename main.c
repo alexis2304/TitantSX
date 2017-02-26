dd2c54a8d37910b507cd67bcf7fd7903115b9f16
Alexis Gougaut <alex@alexiss-mbp.home>
2017-02-26 11:47:49 +0100

    Update

--- main.c
+++ main.c
@@ -1,70 +1,111 @@
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+/* Surely you will remove the processor conditionals and this comment
+   appropriately depending on whether or not you use C++. */
+#if !defined(__cplusplus)
+#include <stdbool.h> /* C doesn't have booleans by default. */
+#endif
+#include <stddef.h>
+#include <stdint.h>
 
-#include <X11/Xlib.h>
+/* Check if the compiler thinks we are targeting the wrong operating system. */
+#if defined(__linux__)
+#error "You are not using a cross-compiler, you will most certainly run into trouble"
+#endif
 
+/* This tutorial will only work for the 32-bit ix86 targets. */
+#if !defined(__i386__)
+#error "This tutorial needs to be compiled with a ix86-elf compiler"
+#endif
 
-int main(int argc, char **argv){
-	Display *d;
-	Window w;
-	XEvent e;
-	int x;		// la position x
-	int y;		// La position y
-	int width;	// La largeur
-	int height;	// La hauteur
+/* Hardware text mode color constants. */
+enum vga_color {
+	VGA_COLOR_BLACK = 0,
+	VGA_COLOR_BLUE = 1,
+	VGA_COLOR_GREEN = 2,
+	VGA_COLOR_CYAN = 3,
+	VGA_COLOR_RED = 4,
+	VGA_COLOR_MAGENTA = 5,
+	VGA_COLOR_BROWN = 6,
+	VGA_COLOR_LIGHT_GREY = 7,
+	VGA_COLOR_DARK_GREY = 8,
+	VGA_COLOR_LIGHT_BLUE = 9,
+	VGA_COLOR_LIGHT_GREEN = 10,
+	VGA_COLOR_LIGHT_CYAN = 11,
+	VGA_COLOR_LIGHT_RED = 12,
+	VGA_COLOR_LIGHT_MAGENTA = 13,
+	VGA_COLOR_LIGHT_BROWN = 14,
+	VGA_COLOR_WHITE = 15,
+};
 
-	x = 0;
-	y = 0;
-	width = 300;
-	height = 300;
+static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg) {
+	return fg | bg << 4;
+}
 
-	// La colormap pour alouer les couleurs
-	Colormap screen_colormap;
+static inline uint16_t vga_entry(unsigned char uc, uint8_t color) {
+	return (uint16_t) uc | (uint16_t) color << 8;
+}
 
-	// Crée les valeur de couleurs
-	XColor col;
+size_t strlen(const char* str) {
+	size_t len = 0;
+	while (str[len])
+		len++;
+	return len;
+}
 
-	char* msg = "Hello World!";
-	int s;
+static const size_t VGA_WIDTH = 80;
+static const size_t VGA_HEIGHT = 25;
 
-	d = XOpenDisplay(NULL);
-	if(d == NULL){
-		fprintf(stderr, "Cannot open display\n");
-		exit(1);
-	}
-	width = DisplayWidth(d, 0);
-	height = DisplayHeight(d, 0);
-	s = DefaultScreen(d);
-	w = XCreateSimpleWindow(d, RootWindow(d, s), x, y, width, height, 0,
-					BlackPixel(d, s), WhitePixel(d, s));
-	XSelectInput(d, w,  ExposureMask | KeyPressMask);
-	XMapWindow(d, w);
-    XClearWindow(d, w);
+size_t terminal_row;
+size_t terminal_column;
+uint8_t terminal_color;
+uint16_t* terminal_buffer;
 
-	printf("Screen Width : %d\n", DisplayWidth(d, 0));
-	printf("Screen Height : %d\n", DisplayHeight(d, 0));
-
-	screen_colormap = DefaultColormap(d, s);
-	Status rc;
-	XParseColor(d, screen_colormap, "#000000", &col);
-	XAllocColor(d, screen_colormap, &col);
-	while(1){
-		XNextEvent(d, &e);
-		if(e.type == Expose){
-			XSetForeground(d, DefaultGC(d, s), col.pixel);
-			XFillRectangle(d, w, DefaultGC(d, s), 20, 20, 10, 10);
+void terminal_initialize(void) {
+	terminal_row = 0;
+	terminal_column = 0;
+	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
+	terminal_buffer = (uint16_t*) 0xB8000;
+	for (size_t y = 0; y < VGA_HEIGHT; y++) {
+		for (size_t x = 0; x < VGA_WIDTH; x++) {
+			const size_t index = y * VGA_WIDTH + x;
+			terminal_buffer[index] = vga_entry(' ', terminal_color);
 		}
-		if(e.type == KeyPress){
-			x++;
-			XMoveWindow(d, w, x, y);
-			printf("Key %d pressed !\n", e.xkey.keycode);
-			if(e.xkey.keycode == 9)
-				break;
-		}
-		XFlush(d);
 	}
-	XCloseDisplay(d);
-	return 0;
+}
+
+void terminal_setcolor(uint8_t color) {
+	terminal_color = color;
+}
+
+void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) {
+	const size_t index = y * VGA_WIDTH + x;
+	terminal_buffer[index] = vga_entry(c, color);
+}
+
+void terminal_putchar(char c) {
+	terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
+	if (++terminal_column == VGA_WIDTH) {
+		terminal_column = 0;
+		if (++terminal_row == VGA_HEIGHT)
+			terminal_row = 0;
+	}
+}
+
+void terminal_write(const char* data, size_t size) {
+	for (size_t i = 0; i < size; i++)
+		terminal_putchar(data[i]);
+}
+
+void terminal_writestring(const char* data) {
+	terminal_write(data, strlen(data));
+}
+
+#if defined(__cplusplus)
+extern "C" /* Use C linkage for kernel_main. */
+#endif
+void kernel_main(void) {
+	/* Initialize terminal interface */
+	terminal_initialize();
+
+	/* Newline support is left as an exercise. */
+	terminal_writestring("Hello, kernel World!\n");
 }
